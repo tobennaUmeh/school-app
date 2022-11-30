5a81fc1a68f1d10e6fcc06786e23eaeda5580f56
tobennaUmeh <94992053+tobennaUmeh@users.noreply.github.com>
2022-11-30 09:54:40 +0100

    page creation

--- School-app/src/App.jsx
+++ School-app/src/App.jsx
@@ -1,45 +1,19 @@
-import { useState } from 'react';
 import './App.css';
-import Table from 'react-bootstrap/Table';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Create from './pages/CreatePage/Create';
+import ViewPage from './pages/ViewPage/ViewPage';
+import LandingPage from './pages/landingPage/LandingPage';
 
 function App() {
 	return (
 		<div className='App'>
-			<Table striped>
-				<thead>
-					{data.map((item) => (
-						<tr>
-							<th>{item.name}</th>
-							<th>{item.age}</th>
-						</tr>
-					))}
-					<tr>
-						<th>#</th>
-						<th>First Name</th>
-						<th>Last Name</th>
-						<th>Username</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>1</td>
-						<td>Mark</td>
-						<td>Otto</td>
-						<td>@mdo</td>
-					</tr>
-					<tr>
-						<td>2</td>
-						<td>Jacob</td>
-						<td>Thornton</td>
-						<td>@fat</td>
-					</tr>
-					<tr>
-						<td>3</td>
-						<td colSpan={2}>Larry the Bird</td>
-						<td>@twitter</td>
-					</tr>
-				</tbody>
-			</Table>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<LandingPage />} />
+					<Route path='/create' element={<Create />} />
+					<Route path='/view' element={<ViewPage />} />
+				</Routes>
+			</BrowserRouter>
 		</div>
 	);
 }
